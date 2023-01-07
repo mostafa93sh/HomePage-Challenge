4099b9430eb111490a600450a56618d1ae4297d7
mostafa93sh <mostafa.khalel.93@gmail.com>
2023-01-07 06:49:36 +0200

    complete mobile view functionality

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@ const mobilImageIcone = document.getElementById("mobile-img");
 console.log(mobilImageIcone);
 mobilImageIcone.addEventListener("click", function (e) {
   console.log("HELLO WORLD", e.target.currentSrc);
-  const wantedPath = "http://127.0.0.1:5500/assets/images/icon-menu-close.svg";
-  mobilImageIcone.src = wantedPath;
-  const hiddenList = document.getElementsByClassName("hidden-navbar")[0];
-  hiddenList.classList.replace("hidden-navbar", "d-flex-colum");
+  let wantedPath = "http://127.0.0.1:5500/assets/images/icon-menu-close.svg";
+  if (e.target.currentSrc === wantedPath) {
+    const hiddenList = document.getElementsByClassName("d-flex-colum")[0];
+    wantedPath = "http://127.0.0.1:5500/assets/images/icon-menu.svg";
+    hiddenList.classList.replace("d-flex-colum", "hidden-navbar");
+    console.log("CASE ONE");
+    mobilImageIcone.src = wantedPath;
+  } else {
+    const hiddenList = document.getElementsByClassName("hidden-navbar")[0];
+    console.log("CASE TWO");
+    mobilImageIcone.src = wantedPath;
+    hiddenList.classList.replace("hidden-navbar", "d-flex-colum");
+  }
 });
+
+// document
+//   .getElementsByClassName("d-flex-colum")[0]
+//   .addEventListener("click", function () {
+//     mobilImageIcone.src = "http://127.0.0.1:5500/assets/images/icon-menu.svg";
+//   });
